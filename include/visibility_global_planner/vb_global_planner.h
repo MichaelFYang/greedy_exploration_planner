3685e543bee7c9781ddd778427783005e83f351f
Michael <fanyang2@andrew.cmu.edu>
2019-09-30 18:07:21 -0400

    09-30, 6:07pm

--- include/visibility_global_planner/vb_global_planner.h
+++ include/visibility_global_planner/vb_global_planner.h
@@ -15,6 +15,24 @@ typedef geometry_msgs::PointStamped ROSWayPoint;
 struct Point {
     float x;
     float y;
+    float operator *(const Point& pt) const
+    {
+        return x * pt.x + y * pt.y;
+    }
+    Point operator +(const Point& pt) const
+    {
+        Point result;
+        result.x += pt.x;
+        result.y += pt.y;
+        return result;
+    }
+    Point operator -(const Point& pt) const
+    {
+        Point result;
+        result.x -= pt.x;
+        result.y -= pt.y;
+        return result;
+    }
 };
 
 
@@ -31,19 +49,26 @@ private:
     ros::Subscriber odom_sub_;
     ros::Publishder goal_pub_;
     // function define
+    Point CPoint(float x, float y);
+    float Norm(Point p);
     void PrincipalAnalysis();
     void ElasticRawCast();
     void InitializeParam();
     void OdomHandler(const nav_msgs::Odometry odom_msg);
     ROSWayPoint HandleWaypoint();
     void CloudHandler(const sensor_msgs::PointCloud2ConstPtr cloud_msg);
+    bool HitObstacle(Point p);
     // valuable define
     nav_msgs::Odometry odom_;
     Point robot_heading_;
+    Point robot_center_;
     ROSPoint goal_waypoint_;
-    ROSPoint principal_direction_;
+    Point principal_direction_;
     pcl::PointCloud<pcl::PointXYZI>::Ptr laser_cloud_(new pcl::PointCloud<pcl::PointXYZI>());
-
+    // ros  parameter value
+    int raw_cast_revolution_;
+    float max_sensor_range_;
+    int obs_count_thred_;
 
 
 }
--- src/vb_global_planner.cpp
+++ src/vb_global_planner.cpp
@@ -3,9 +3,21 @@
 
 /* ---------------------------------------------------------------------------- */
 
-VB_Planner::VB_Planner() {}
+VB_Planner::VB_Planner() {
+    // ROS Parameter Read 
+    if (!nh_.getParam("ray_cast_revolution",raw_cast_revolution_)) {
+        raw_cast_revolution_ = 20;
+    }
+    if (!nh_.getParam("max_sensor_range",max_sensor_range_)) {
+        max_sensor_range_ = 100.0;
+    }
+    if (!nh_.getParam("obs_count_thred",obs_count_thred_)) {
+        obs_count_thred_ = 50;
+    }
+}
 
 void VB_Planner::Loop() {
+    // Loop Subscriber
     goal_pub_ = nh_.advertise<geometry_msgs::PointStamped>("_",1);
     point_cloud_sub_ = nh_.subscribe("_",1,&VB_Planner::CloudHandler,this);
     odom_sub_ = nh_.subscribe("_",1,&VB_Planner::OdomHandler,this);
@@ -19,30 +31,59 @@ void VB_Planner::PrincipalAnalysis() {
     pcl::PCA<pcl::PointXYZI> pca(*laser_cloud_);
     eigen_vectors = pca.getEigenVectors();
     Eigen::Vector3f vector;
+
     vector(0) = eigen_vectors(0,0);
     vector(1) = eigen_vectors(0,1);
     vector(2) = eigen_vectors(0,2);
     float norm = sqrt(vector(0)*vector(0) + vector(1)*vector(1));
     principal_direction_.x = vector(0) / norm;
     principal_direction_.y = vector(1) / norm;
-    // TODO !!!
-    
+    if (principal_direction_ * robot_heading_ < 0) {
+        principal_direction_.x *= -1;
+        principal_direction_.x *= -1;
+    }
 }
 
 void VB_Planner::ElasticRawCast() {
     // Input: PointCloud; and Principal direction vector
     // Output: update goal waypoint -- the travel distance to an obstacle
+    int counter = 0;
+    float center_x = odom_.pose.pose.position.x;
+    float center_y = odom_.pose.pose.position.y;
+    Point center_pos = this->CPoint(center_x, center_y)
+    Point check_pos = center_pos;
+    while(counter < obs_count_thred_ && this->Norm(check_pos - center_pos) < max_sensor_range_) {
+        check_pos += principal_direction_;
+        if (this->HitObstacle(check_pos)) {
+            counter += 1;
+        }
+    }
+    goal_waypoint_.point.x = check_;point.x;
+    goal_waypoint_.point.y = check_;point.y;
+}
+
+bool VB_Planner::HitObstacle(Point p) {
+    return false;
 }
 
 void VB_Planner::OdomHandler(const nav_msgs::Odometry odom_msg) {
+    // Credit: CMU SUB_T dfs_behavior_planner, Chao C.,
+    // https://bitbucket.org/cmusubt/dfs_behavior_planner/src/master/src/dfs_behavior_planner/dfs_behavior_planner.cpp
+
     odom_ = odom_msg;
     double roll, pitch, yaw;
     geometry_msgs::Quaternion geo_quat = odometry_msg->pose.pose.orientation;
     tf::Matrix3x3(tf::Quaternion(geo_quat.x, geo_quat.y, geo_quat.z, geo_quat.w)).getRPY(roll, pitch, yaw);
-    robot_heading.x = cos(yaw);
-    robot_heading.y = sin(yaw);
+    robot_heading_.x = cos(yaw);
+    robot_heading_.y = sin(yaw);
 
+}
 
+Point VB_Planner::CPoint(float x, float y) {
+    Point p;
+    p.x = x;
+    p.y = y;
+    return p;
 }
 
 
